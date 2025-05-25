e1296f994d4bad892eaccb887141faf6dfb7ebd7
Pranesh <praneshmadhan646@gmail.com>
2025-05-25 21:15:47 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-699793da8ff96e910ecb.js"
+    "url": "webpack-runtime-83b6ea6819e7acc26c06.js"
   },
   {
     "url": "framework-22d51f0f5ee9f1c9c6e1.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "aba011d20a0ba37ce9a3d71f4f3d225b"
+    "revision": "031ad85f06e2abc97d820bcf20c47f24"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-49be599b1ad8fa2a8465.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "5f3d89ff760ba38662289288b67d872d"
+    "revision": "8ae17ea1dcbc9d7a9768012d78bf2ce3"
   },
   {
     "url": "polyfill-1d1b2ce50066059b8c94.js"
