9ea63ff6f858f57511095d1c2f8ca754e5cb9980
Pranesh <praneshmadhan646@gmail.com>
2025-09-10 11:32:46 +0530

    changed gif to play video in loop

--- src/components/sections/jobs.js
+++ src/components/sections/jobs.js
@@ -1,149 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
-import sr from '@utils/sr';
-import { srConfig } from '@config';
-import styled from 'styled-components';
-import { theme, mixins, media, Section, Heading } from '@styles';
-const { colors, fontSizes, fonts } = theme;
-
-const StyledContainer = styled(Section)`
-  position: relative;
-  max-width: 700px;
-`;
-const StyledTabs = styled.div`
-  display: flex;
-  align-items: flex-start;
-  position: relative;
-  ${media.thone`
-    display: block;
-  `};
-`;
-const StyledTabList = styled.ul`
+const StyledMedia = styled.video`
   display: block;
-  position: relative;
-  width: max-content;
-  z-index: 3;
-  padding: 0;
-  margin: 0;
-  list-style: none;
-
-  ${media.thone`
-    display: flex;
-    overflow-x: scroll;
-    margin-bottom: 30px;
-    width: calc(100% + 100px);
-    margin-left: -50px;
-  `};
-  ${media.phablet`
-    width: calc(100% + 50px);
-    margin-left: -25px;
-  `};
-
-  li {
-    &:first-of-type {
-      ${media.thone`
-        margin-left: 50px;
-      `};
-      ${media.phablet`
-        margin-left: 25px;
-      `};
-    }
-    &:last-of-type {
-      ${media.thone`
-        padding-right: 50px;
-      `};
-      ${media.phablet`
-        padding-right: 25px;
-      `};
-    }
-  }
-`;
-const StyledTabButton = styled.button`
-  ${mixins.link};
-  display: flex;
-  align-items: center;
-  width: 100%;
-  background-color: transparent;
-  height: ${theme.tabHeight}px;
-  padding: 0 20px 2px;
-  transition: ${theme.transition};
-  border-left: 2px solid ${colors.lightestNavy};
-  text-align: left;
-  white-space: nowrap;
-  font-family: ${fonts.SFMono};
-  font-size: ${fontSizes.smish};
-  color: ${props => (props.isActive ? colors.green : colors.slate)};
-  ${media.tablet`padding: 0 15px 2px;`};
-  ${media.thone`
-    ${mixins.flexCenter};
-    padding: 0 15px;
-    text-align: center;
-    border-left: 0;
-    border-bottom: 2px solid ${colors.lightestNavy};
-    min-width: 120px;
-  `};
-  &:hover,
-  &:focus {
-    background-color: ${colors.lightNavy};
-  }
-`;
-const StyledHighlight = styled.span`
-  display: block;
-  background: ${colors.green};
-  width: 2px;
-  height: ${theme.tabHeight}px;
-  border-radius: ${theme.borderRadius};
-  position: absolute;
-  top: 0;
-  left: 0;
-  transition: transform 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
-  transition-delay: 0.1s;
-  z-index: 10;
-  transform: translateY(
-    ${props => (props.activeTabId > 0 ? props.activeTabId * theme.tabHeight : 0)}px
-  );
-  ${media.thone`
-    width: 100%;
-    max-width: ${theme.tabWidth}px;
-    height: 2px;
-    top: auto;
-    bottom: 0;
-    transform: translateX(
-      ${props => (props.activeTabId > 0 ? props.activeTabId * theme.tabWidth : 0)}px
-    );
-    margin-left: 50px;
-  `};
-  ${media.phablet`
-    margin-left: 25px;
-  `};
-`;
-const StyledTabContent = styled.div`
-  position: relative;
-  width: 100%;
-  height: auto;
-  padding-top: 12px;
-  padding-left: 0; // Remove left padding to eliminate the line
-  ${media.tablet`padding-left: 0;`};
-  ${media.thone`padding-left: 0;`};
-
-  ul {
-    ${mixins.fancyList};
-  }
-  a {
-    ${mixins.inlineLink};
-  }
-`;
-const StyledJobTitle = styled.h4`
-  color: ${colors.lightestSlate};
-  font-size: ${fontSizes.xxl};
-  font-weight: 500;
-  margin-bottom: 5px;
-`;
-
-const StyledGif = styled.img`
-  display: block;
-  margin: 32px auto 0 auto; // Adds space above gif and centers it
+  margin: 32px auto 0 auto;
   max-width: 400px;
+  width: 100%;
+  border-radius: ${theme.borderRadius}px;
+  background-color: ${colors.lightNavy};
 `;
 
 const Skills = ({ data }) => {
@@ -216,9 +77,14 @@ const Skills = ({ data }) => {
                   <span>{title}</span>
                 </StyledJobTitle>
                 {gif && gif.publicURL && (
-                  <StyledGif
+                  <StyledMedia
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
                     src={gif.publicURL}
-                    alt={`${title} gif`}
+                    type="video/mp4"
+                    aria-label={`${title} video demonstration`}
                   />
                 )}
                 <div dangerouslySetInnerHTML={{ __html: html }} />
